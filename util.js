9a048d13437ac68d16eba09d589f5132934a9dcd
vlados-a <azarkovlad@gmail.com>
2015-12-26 22:36:06 +0300

    Learn util.format

--- util.js
+++ util.js
@@ -1,5 +1,6 @@
 var util = require('util');
 
+/*util.inspect*/
 var o = {
 	a: 12,
 	b: 14,
@@ -13,3 +14,8 @@ var o = {
 o.self = o;
 var s = util.inspect(o);
 console.log(s); // the same as console.log(o);
+
+
+/*util.format*/
+var s = util.format("My name is %s. I'm %d years old. My json description is: %j", "Uladzislau", 20, {name: "Uladzislau", age: 20});
+console.log(s);// the same as console.log("My name is %s. I'm %d years old. My json description is: %j", "Uladzislau", 20, {name: "Uladzislau", age: 20});
