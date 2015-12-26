56652f84eb0680d93778795aa29fb82651c2639f
vlados-a <azarkovlad@gmail.com>
2015-12-26 22:42:35 +0300

    Learn util.inherits

--- util.js
+++ util.js
@@ -19,3 +19,26 @@ console.log(s); // the same as console.log(o);
 /*util.format*/
 var s = util.format("My name is %s. I'm %d years old. My json description is: %j", "Uladzislau", 20, {name: "Uladzislau", age: 20});
 console.log(s);// the same as console.log("My name is %s. I'm %d years old. My json description is: %j", "Uladzislau", 20, {name: "Uladzislau", age: 20});
+
+/*util inherits*/
+
+function Animal(name){
+	this.name = name;
+}
+
+Animal.prototype.walk = function(){
+	console.log("Animal %s is walking", this.name);
+};
+
+function Rabbit(name){
+	this.name = name;
+}
+util.inherits(Rabbit, Animal);
+
+Rabbit.prototype.jump = function(){
+	console.log("Rabbit %s is jumping", this.name);
+}
+
+var rabbit = new Rabbit("Rob");
+rabbit.walk();
+rabbit.jump();
