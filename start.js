f94bfa83c3893fd6e291287e3dc9c7406bde5759
vlados-a <azarkovlad@gmail.com>
2015-12-26 16:19:06 +0300

    Using module.parent

--- start.js
+++ start.js
@@ -1,6 +1,15 @@
 var user = require('./user');
 
-var tom = new user.User("Tom");
-var bob = new user.User("Bob");
+var  run = function(){
+	var tom = new user.User("Tom");
+	var bob = new user.User("Bob");
 
-tom.hello(bob);
+	tom.hello(bob);
+}
+
+if(module.parent){
+	eports.run = run;
+}
+else{
+	run();
+}
