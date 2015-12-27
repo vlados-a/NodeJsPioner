8bcb79d8f489c0d503a9945315bb46bc1019486c
vlados-a <azarkovlad@gmail.com>
2015-12-27 13:15:52 +0300

    Add logging of server events

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var server = new http.Server();
 
 server.listen(1337, '127.0.0.1');
 
+var emit = server.emit;
+server.emit = function(event){
+	console.log(event);
+	emit.apply(server, arguments);
+}
+
+
 var counter = 0;
 server.on('request', function(req, res){
 	res.end(util.format("Hello, World! Request count: %d", ++counter));
