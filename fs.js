5ea2013218df8a0d01b24c452250e1dffea19fe7
vlados-a <azarkovlad@gmail.com>
2016-01-02 18:04:31 +0300

    Write streams, method pipe

--- fs.js
+++ fs.js
@@ -1,16 +1,36 @@
-var fs = require('fs');
+var fs = require('fs'),
+	http = require('http');
 
-var stream = new fs.ReadStream("dfd");
-
-stream.on('readable', function(){
-	var data = stream.read();
-	if(data){
-		console.log(data.toString());
+var server = http.createServer(function(req, res){
+	if(req.url = "index.html"){
+		sendFile(new fs.ReadStream("index.html"), res);
 	}
-})
-stream.on('end', function(){
-	console.log('The End!!!');
-})
-stream.on('error', function(){
-	console.log('Ops, some error');
-})
+}).listen(1337, '127.0.0.1');
+
+function sendFile(file, res){
+	/*The code below is equal to file.pipe(res);*/
+	file.pipe(res);
+	file.pipe(process.stdout);
+	file.on('error', function(){
+		res.statusCode = 500;
+		res.end('Internal server error');
+		return;
+	});
+	res.on('close', function(){
+		file.destroy();
+	})
+	/*file.on('readable', write);
+	file.on('end', function(){
+		res.end();
+	});
+	function write(data){
+		var data = file.read();
+		if(data && !res.write(data)){
+			file.removeListener('readable', write);
+			res.once('drain', function(){
+				file.on('readable', write);
+				write();
+			});
+		}
+	}*/
+}
