9a5929be84153a8abb4f2bd1816dd17ccb2dac58
vlados-a <azarkovlad@gmail.com>
2016-01-02 14:29:36 +0300

    Example of async processing

---
+++ async.js
@@ -0,0 +1,14 @@
+var fs = require('fs'),
+	http = require('http');
+
+http.createServer(function(req, res){
+	if(req.url = '/'){
+		fs.readFile('index.html', function(err, file){
+			if(!err){
+				res.end(file);
+			}
+			res.statusCode = 500;
+			res.end('File not found');
+		})
+	}
+}).listen(1337, '127.0.0.1');
