0480e508e0309a4e1f398689db0bf9751b2cc41e
vlados-a <azarkovlad@gmail.com>
2016-01-02 23:13:55 +0300

    Add server to domain to avoid possible errors

--- domain.js
+++ domain.js
@@ -1,10 +1,10 @@
-var domain = require('domain'),
-	server = require('./server');
+var domain = require('domain');
 
 var serverDomain = domain.create();
 serverDomain.on('error', function(e){
 	console.log('domain catch error: %s', e.toString());
 });
 serverDomain.run(function(){
+	server = require('./server');
 	server.listen(1337, '127.0.0.1');
 });
