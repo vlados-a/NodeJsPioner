ea86302418218d53d029dadaffd9aca34d567170
vlados-a <azarkovlad@gmail.com>
2016-01-08 13:10:10 +0300

    Add error handling

--- chat/app.js
+++ chat/app.js
@@ -19,6 +19,52 @@ app.configure(function(){
   app.use(express.session({ secret: 'your secret here' }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  app.use(function(req, res, next){
+    if(req.url == '/home'){
+      res.end('you are on home page')
+    }
+    else{
+      next();
+    } 
+  });
+  app.use(function(req, res, next){
+    if(req.url == '/admin'){
+      res.end('you are on admin page');
+    }
+    else{
+      next();
+    }
+  });
+  app.use(function(req, res, next){
+    if(req.url == '/error'){
+      bla();
+    }
+    else{
+      next();
+    }
+  });
+  app.use(function(req, res, next){
+    if(req.url == '/forbiden'){
+      next(new Error('Access denied'));
+    }
+    else{
+      next();
+    }
+  });
+  app.use(function(req, res){
+    res.send(404, 'Page not found');
+  });
+  app.use(function(err, req, res, next){
+    console.log(app.get('env'));
+    if(app.get('env') == 'development'){
+      res.end('Sorry. Some error ' + err.message);  
+    }
+    else{
+      var handler = express.errorHandler();
+      handler(err, req, res, next);
+    }
+    
+  });
 });
 
 app.configure('development', function(){
