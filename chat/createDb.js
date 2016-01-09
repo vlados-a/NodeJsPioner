b2106bd10f5e647c435d3d65638694e37e930acb
= <=>
2016-01-09 13:07:37 +0300

    Add users to the db via async

--- chat/createDb.js
+++ chat/createDb.js
@@ -1,12 +1,35 @@
-var User = require('models/user').User;
+var User = require('models/user').User,
+    mongoose = require('libs/mongoose'),
+    asyn = require('async');
 
-var vlad = new User({
-  username: 'Vlad',
-  password: 'secret'
-});
+mongoose.connection.on('open', function(){
+  var db = mongoose.connection.db;
 
-vlad.save(function(err, effected){
-  if(!err) console.log('success');
-
-  console.log(effected);
-});
+  db.dropDatabase(function(err){
+    if(err) throw err;
+    console.log('OK');
+    asyn.parallel([
+      function(callback){
+        var vlad = new User({username: 'Vlad', password: 'secret'});
+        vlad.save(function(err){
+          callback(err, vlad);
+        });
+      },
+      function(callback){
+        var yura = new User({username: 'Yura', password: 'secret'});
+        yura.save(function(err){
+          callback(err, yura);
+        });
+      },
+      function(callback){
+        var olya = new User({username: 'Olya', password: 'secret'});
+        olya.save(function(err){
+          callback(err, olya);
+        });
+      }
+    ], function(err, results){
+        console.log(results);
+        mongoose.disconnect();
+    });
+  });
+})
