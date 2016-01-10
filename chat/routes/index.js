6657c5fd47bdc5e4fb7c96ba1b8ef2445a356465
= <=>
2016-01-10 13:55:08 +0300

    Move users actions to users route file

--- chat/routes/index.js
+++ chat/routes/index.js
@@ -1,39 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-var HttpError = require('../error').HttpError,
-    user = require('models/user').User,
-    ObjectId = require('mongodb').ObjectId;
+
 
 /* GET home page. */
-/*router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});*/
-
-router.get('/usersInfo', function(req,res,next){
-  user.find({}, function(err, data){
-    if(err) next(err);
-    res.json(data);
-  });
-});
-router.get('/usersInfo/:id', function(req, res, next){
-  try{
-    var id = new ObjectId(req.params.id);
-  }
-  catch(e){
-    next(new HttpError(404, 'Incorrect user id'));
-  }
-  user.findById(id, function(err, data){
-    if(err) next(err);
-    else{
-      if(!data){
-        next(new HttpError(404, 'User not found'));
-      }
-      else{
-        res.json(data);
-      }
-    }
-  });
 });
 
 module.exports = router;
--- chat/routes/users.js
+++ chat/routes/users.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+var HttpError = require('../error').HttpError,
+    user = require('models/user').User,
+    ObjectId = require('mongodb').ObjectId;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  user.find({}, function(err, data){
+    if(err) next(err);
+    res.json(data);
+  });
 });
 
+router.get('/:id', function(req, res, next){
+  try{
+    var id = new ObjectId(req.params.id);
+  }
+  catch(e){
+    next(new HttpError(404, 'Incorrect user id'));
+  }
+  user.findById(id, function(err, data){
+    if(err) next(err);
+    else{
+      if(!data){
+        next(new HttpError(404, 'User not found'));
+      }
+      else{
+        res.json(data);
+      }
+    }
+  });
+});
 module.exports = router;
